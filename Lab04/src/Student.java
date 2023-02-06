86d5843ecc16ed96d5a25195c40cf3952f20f2dc
Scorpionzaa555 <74282946+Scorpionzaa555@users.noreply.github.com>
2023-02-06 23:44:47 +0700

    Add files via upload

---
+++ Lab04/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    public String name;
+    public double mScore, fScore;
+    public void showGrade () {
+        double score = (mScore*0.4)+(fScore*0.4)+20;
+        System.out.println("Your grade : " + score);
+    }
+}
