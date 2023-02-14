7b7543a05341f887963e60313ea3789decc4b2d0
Scorpionzaa555 <74282946+Scorpionzaa555@users.noreply.github.com>
2023-02-14 19:32:32 +0700

    Delete Lab01 directory

--- Lab01/src/ChangeAD.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class ChangeAD {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int BE = sc.nextInt(), AD;
-        AD = BE -543;
-        System.out.println(AD);
-    }
-}
