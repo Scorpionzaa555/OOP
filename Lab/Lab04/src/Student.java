c64b877372184e3ac3166684ee9b740479a25d00
Thanawan Chaemsathian <74282946+Scorpionzaa555@users.noreply.github.com>
2023-03-04 15:56:38 +0700

    Update Student.java

--- Lab/Lab04/src/Student.java
+++ Lab/Lab04/src/Student.java
@@ -1,6 +1,7 @@
 public class Student {
     public String name;
-    public double mScore, fScore;
+    public double mScore;
+    public double fScore;
     public void showGrade () {
         double score = (mScore*0.4)+(fScore*0.4)+20;
         System.out.println("Your grade : " + score);
