375d847cab7ee1bb41b8c0644d06d7d2a559308e
Thanawan Chaemsathian <74282946+Scorpionzaa555@users.noreply.github.com>
2023-03-31 15:51:32 +0700

    Update TellerGUI.java

--- Lab/Lab10/src/TellerGUI.java
+++ Lab/Lab10/src/TellerGUI.java
@@ -6,7 +6,6 @@ public class TellerGUI {
     private JFrame f;
     private JPanel panel1;
     private JPanel panel2;
-    private JPanel panel3;
     private JLabel label1;
     private JLabel label2;
     private TextField tf1;
@@ -17,36 +16,36 @@ public class TellerGUI {
     
     public TellerGUI() {
         
-        panel1 = new JPanel();
-        panel2 = new JPanel();
-        panel3 = new JPanel();
         f = new JFrame("Teller GUI");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(315, 240);
+        f.setSize(315, 170);
+        panel1 = new JPanel();
+        panel2 = new JPanel();
         
-        f.setLayout(new GridLayout(4, 0));
+        f.setLayout(new GridLayout(2, 1));
         label1 = new JLabel("Balance");
         tf1 = new TextField("6000");
+        tf1.setEnabled(false);
+        
         label2 = new JLabel("Amount");
         tf2 = new TextField();
         
-        panel1.setLayout(new BorderLayout());
-        panel2.setLayout(new BorderLayout());
+        panel1.setLayout(new GridLayout(2, 2));
+        panel2.setLayout(new FlowLayout());
         
-        panel1.add(label1, BorderLayout.WEST);
-        panel1.add(tf1, BorderLayout.EAST);
-        panel2.add(label2, BorderLayout.WEST);
-        panel2.add(tf2, BorderLayout.EAST);
+        panel1.add(label1);
+        panel1.add(tf1);
+        panel1.add(label2);
+        panel1.add(tf2);
         
         b1 = new JButton("Deposit");
         b2 = new JButton("Withdraw");
         b3 = new JButton("Exit");
-        panel3.add(b1);
-        panel3.add(b2);
-        panel3.add(b3);
+        panel2.add(b1);
+        panel2.add(b2);
+        panel2.add(b3);
         f.add(panel1);
         f.add(panel2);
-        f.add(panel3);
         
         f.setVisible(true);
     }
