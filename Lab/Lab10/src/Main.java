e97c7a9a15c4c43abc1183fce8b0cd2629e92ce9
Thanawan Chaemsathian <74282946+Scorpionzaa555@users.noreply.github.com>
2023-04-05 16:01:40 +0700

    Update Main.java

--- Lab/Lab10/src/Main.java
+++ Lab/Lab10/src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //new CalculatorOneGUI();
         //new CalculatorTwoGUI();
-        new TellerGUI();
+        //new TellerGUI();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(() -> {MDIFromGUI f1 = new MDIFromGUI(); });
     }
 }
